b177bcc57767cb65fdf259dd227e520a129c9f54
Cole Sammons <colemsammons@gmail.com>
2023-03-03 13:03:53 -0700

    initial commit

---
+++ practice.php
@@ -0,0 +1,16 @@
+<!-- echo -->
+<h1>
+    <?php
+    $var = 'Hello World';
+    echo $var;
+    ?>
+</h1>
+<!-- shorthand echo -->
+<?='<p>Hello World</p>'; ?>
+
+<!-- Concat two strings -->
+<?php echo 'Hello, ' . $name; ?>
+<!-- Or place it inside "" -->
+<?php
+echo "Hello, $name"
+    ?>
